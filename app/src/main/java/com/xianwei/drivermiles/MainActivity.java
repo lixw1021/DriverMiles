38e3a0ecd379798d250c06db9600d4423df390ce
xianwei li <lixw1021@gmail.com>
2018-03-29 22:12:22 -0700

    complete login UI

--- app/src/main/java/com/xianwei/drivermiles/MainActivity.java
+++ app/src/main/java/com/xianwei/drivermiles/MainActivity.java
@@ -2,6 +2,10 @@ package com.xianwei.drivermiles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+    }
+
+    @OnClick(R.id.btn_create_account)
+    void createAccount(){
+
     }
 }
